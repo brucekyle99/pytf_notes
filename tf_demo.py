d815b942039da5b1328df236d578d4b798cf00dc
brucekyle99 <31788908+brucekyle99@users.noreply.github.com>
2019-09-30 09:32:48 +0800

    Create tf_demo.py

---
+++ tf_demo.py
@@ -0,0 +1,11 @@
+
+def conv_relu(input, kernel_shape, bias_shape):
+    # Create variable named "weights".
+    weights = tf.get_variable("weights", kernel_shape,
+        initializer=tf.random_normal_initializer())
+    # Create variable named "biases".
+    biases = tf.get_variable("biases", bias_shape,
+        initializer=tf.constant_initializer(0.0))
+    conv = tf.nn.conv2d(input, weights,
+        strides=[1, 1, 1, 1], padding='SAME')
+    return tf.nn.relu(conv + biases)
